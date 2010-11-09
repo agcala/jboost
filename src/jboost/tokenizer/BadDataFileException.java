8b709a01b50244f7854da8821c3025dea37153ea
scheaman <scheaman@localhost>
2010-11-09 13:04:38 -0800

    remove old .java files

--- src/jboost/tokenizer/BadDataFileException.java
+++
@@ -1,8 +0,0 @@
-package jboost.tokenizer;
-
-class BadDataFileException extends ParseException {
-
-  public BadDataFileException(String errorMessage, long lineNum) {
-    super(errorMessage, lineNum);
-  }
-}
